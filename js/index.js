83f378fca0ebd3d56e1d6caaf1a553476bea31e3
Giovanna Formaio <130701136+formaiogih@users.noreply.github.com>
2024-09-10 08:39:53 -0300

    atualizados index.html e js/index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { connect, Schema, model } from 'mongoose';
 import pkg from 'body-parser';
+import cors from 'cors';
+const pag = express()
+pag.use(cors())
+// import axios from 'axios';
 
 const { json } = pkg;
 
@@ -9,26 +13,12 @@ const port = process.env.PORT || 3000;
 
 app.use(json());
 
-// import cors from 'cors'; 
-
-// app.use(cors({
-// origin: 'http://localhost:8080'
-// }));
-
-
-// Conectando ao MongoDB Atlas
 connect('mongodb+srv://Giovanna:megsnoop23@netflixai.muxslkg.mongodb.net/netflixaiDB?retryWrites=true&w=majority&appName=Netflixai')
     .then(() => {
         console.log('Conectado ao MongoDB Atlas');
-
-        // Teste de inserção simples para verificar a conectividade
-        ChatHistory.create({ user: 'TestUser', message: 'TestMessage' })
-            .then(() => console.log('Teste de inserção bem-sucedido'))
-            .catch(err => console.error('Erro na inserção de teste:', err));
     })
     .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
-// Definindo o esquema e o modelo para histórico de chat
 const chatSchema = new Schema({
     user: String,
     message: String,
@@ -55,32 +45,8 @@ app.post('/api/saveChatHistory', async (req, res) => {
         res.status(200).send('Histórico salvo com sucesso!');
     } catch (error) {
         console.error('Erro ao salvar o histórico:', error);
-        res.status(500).send('Erro ao salvar o histórico:', error);
+        res.status(500).send('Erro ao salvar o histórico');
     }
 });
 
-// Inicia o servidor
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-});
 
-// Função para salvar o histórico de chat via requisição fetch
-async function saveChatHistory(user, message) {
-    try {
-        const response = await fetch('http://localhost:3000/api/saveChatHistory', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ user, message })
-        });
-
-        if (response.ok) {
-            console.log('Histórico salvo com sucesso');
-        } else {
-            console.error('Falha ao salvar o histórico');
-        }
-    } catch (error) {
-        console.error('Erro na requisição:', error);
-    }
-}
